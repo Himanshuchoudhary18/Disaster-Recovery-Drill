4a46ea85bfee91b02a7771e7a9bc4b0fb67cedd0
Naman Mishra <reachoutnaman@gmail.com>
2023-07-08 23:11:24 +0530

    added home navigation

--- src/views/drdrill/src/components/Header.js
+++ src/views/drdrill/src/components/Header.js
@@ -5,7 +5,7 @@ import companyLogo from '../img/punjab-national-bank-1024x576.jpg';
 function Header() {
   return (
   <div className='header'>
-    <img src={companyLogo} clasname ='logo'alt="Company Logo"/>
+    < a href="/"><img src={companyLogo} clasname ='logo'alt="Company Logo"/></a>
     <ul className="link">
       <ScrollLink to="about" smooth={true} duration={500}>
             About
